765aa406eed144f601e3b74e2dd832e543f75530
tanlerst <45229323+tanlerst@users.noreply.github.com>
2022-12-08 22:18:37 +0800

    Create 9_SpecialPythagoreanTriplet.cpp

---
+++ attempts/9_SpecialPythagoreanTriplet.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  int a,b,c;
+  for (int i=1;i<(1001/3);i++){
+    for (int j=1;j<(1001/2);j++){
+      int k = 1000-i-j;
+      if ((i*i)+(j*j)==(k*k)){
+        a=i;
+        b=j;
+        c=k;
+        break;
+      }
+    }
+  }
+  cout<<a<<'*'<<b<<'*'<<c<<"="<<a*b*c<<endl;
+}
